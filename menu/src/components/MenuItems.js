8250438f7a0686b6bbfaf00fbea7505806041ff0
ManasKapoor07 <75942485+ManasKapoor07@users.noreply.github.com>
2023-12-19 03:33:52 +0530

    new

---
+++ menu/src/components/MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+    {
+
+        title: 'Furniture',
+        path : '/furniture',
+        cName : 'dropdown-link'
+    },{
+
+        title: 'Electronics',
+        path : '/electronics',
+        cName : 'dropdown-link'
+    },
+    {
+
+        title: 'Travel',
+        path : '/travel',
+        cName : 'dropdown-link'
+    },
+    {
+
+        title: 'Botanical',
+        path : '/botanical',
+        cName : 'dropdown-link'
+    },
+    {
+
+        title: 'Category Name',
+        path : '/category Name',
+        cName : 'dropdown-link'
+    },
+
+]
